de680d293c9ae07db6b134b38c597e95288b2a21
Evan Farnsworth <farnsworth.evan@yahoo.com>
2023-10-05 23:53:01 -0700

    modified js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 async function fetch_projects() {
     let url = `https://sce-development.github.io/Workshop/Website%20Series/Week%203/fakeAPI.json`;
-    let data = await fetch(url).then(resp => resp.json());
+    const data = await fetch(url).then(resp => resp.json());
     console.log(data);
 
-    // var div = document.createElement("div");
-    // div.className = "project";
-
-    // data.map(projectData => `<h1>name<\h1>`)
-    // for(var i = 0; i < data.length; i++){
-    //     div.appendChild(projectData)
-    // }
-
-
-    let container = document.getElementById("project-container");
-    console.log(container)
-    data = data.map(project => `
-        <div class ="project">
-            <h1>${project.name}</h1>
-                <a href="${project.link}>
-                    <img src = ${project.picture}></img>
-                    <p>${project.description}
-                </a?>
-        </div>`);
-    
-    const innerProjects = data.join("\n");
-    container.innerHTML = innerProjects
+    const container = document.getElementById("project-container");
+    data.forEach(project => {
+        const projectElement = document.createElement("div");
+        projectElement.classList.add('project');
+        const projectSkills = project.skills.map(skill => `<li>${skill}</li>`).join("\n");
+        projectElement.innerHTML = `
+            <img src="${project.picture}" alt="${project.name}"/>
+            <h3> <a href="${project.link}" target="_blank">${project.name}</a></h3>
+            <p>${project.description}</p>
+            <ul>${projectSkills}</ul>
+            `;
+        container.appendChild(projectElement);
+    });
 }
+    
+    
+    
+    
+    
+    // console.log(container)
+    // data = data.map(project => `
+    //     <div class ="project">
+    //         <h1>${project.name}</h1>
+    //         <a href="${project.link}>here</a>
+    //         <img src = ${project.picture}></img>
+    //         <p>${project.description}</p>
+    //     </div>`);
+    
+    
+
+    // const innerProjects = data.join("\n");
+    // container.innerHTML = innerProjects
